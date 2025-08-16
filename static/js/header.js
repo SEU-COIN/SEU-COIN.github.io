b526072ab231ed79c9eae927120cd3000b85daaf
Shenyu Zhang <zsy_lookout@outlook.com>
2025-08-17 02:54:27 +0800

    Update

--- static/js/header.js
+++ static/js/header.js
@@ -2,28 +2,35 @@
 function loadHeader() {
   const headerContainer = document.getElementById('header-container');
   if (headerContainer) {
+    // 检测当前页面
+    const currentPage = window.location.pathname.split('/').pop() || 'index.html';
+    const isIndexPage = currentPage === 'index.html';
+    
+    // 根据页面设置不同的字体颜色
+    const navColor = isIndexPage ? '#ffffff' : 'rgb(32, 120, 221)';
+    
     headerContainer.innerHTML = `
-      <header id="header" class="fixed-top ">
+      <header id="header" class="fixed-top " style="padding: 8px 0; min-height: 60px;">
         <div class="container d-flex align-items-center">
 
           <h1 class="logo me-auto" style="display: flex; align-items: center;">
             <a href="index.html"
-              style="font-weight: bold; font-size: 35px; display: flex; align-items: center; text-decoration: none;">
-              <img src="static/img/coin-small.png" alt="Logo" style="height: 40px; margin-right: 10px;">
-              <span style="color:#C9D21E;">CO</span><span style="color:#2DA203;">IN</span>
+              style="font-weight: bold; font-size: 28px; display: flex; align-items: center; text-decoration: none;">
+              <img src="static/img/coin-small.png" alt="Logo" style="height: 32px; margin-right: 8px;">
+              <span style="color:#D6C62E;">CO</span><span style="color:#1D7F3C;">IN</span>
             </a>
           </h1>
 
           <nav id="navbar" class="navbar">
             <ul>
-              <li><a style="color: rgb(32, 120, 221); font-weight: bold; font-size: 23px;" class="nav-link scrollto" href="index.html">Home</a></li>
-              <li><a style="color: rgb(32, 120, 221); font-weight: bold; font-size: 23px;" class="nav-link scrollto" href="news.html">News</a></li>
-              <li><a style="color: rgb(32, 120, 221); font-weight: bold; font-size: 23px;" class="nav-link scrollto" href="members.html">Members</a></li>
-              <li><a style="color: rgb(32, 120, 221); font-weight: bold; font-size: 23px;" class="nav-link scrollto" href="publication.html">Publication</a></li>
-              <li><a style="color: rgb(32, 120, 221); font-weight: bold; font-size: 23px;" class="nav-link scrollto" href="project.html">Project</a></li>
-              <li><a style="color: rgb(32, 120, 221); font-weight: bold; font-size: 23px;" class="nav-link scrollto" href="resources.html">Resources</a></li>
-              <li><a style="color: rgb(32, 120, 221); font-weight: bold; font-size: 23px;" class="nav-link scrollto" href="graduate.html">Graduate</a></li>
-              <li><a style="color: rgb(32, 120, 221); font-weight: bold; font-size: 23px;" class="nav-link scrollto" href="join.html">Join Us</a></li>
+              <li><a style="color: ${navColor}; font-weight: bold; font-size: 18px;" class="nav-link scrollto" href="index.html">Home</a></li>
+              <li><a style="color: ${navColor}; font-weight: bold; font-size: 18px;" class="nav-link scrollto" href="news.html">News</a></li>
+              <li><a style="color: ${navColor}; font-weight: bold; font-size: 18px;" class="nav-link scrollto" href="members.html">Members</a></li>
+              <li><a style="color: ${navColor}; font-weight: bold; font-size: 18px;" class="nav-link scrollto" href="publication.html">Publication</a></li>
+              <li><a style="color: ${navColor}; font-weight: bold; font-size: 18px;" class="nav-link scrollto" href="project.html">Project</a></li>
+              <li><a style="color: ${navColor}; font-weight: bold; font-size: 18px;" class="nav-link scrollto" href="resources.html">Resources</a></li>
+              <li><a style="color: ${navColor}; font-weight: bold; font-size: 18px;" class="nav-link scrollto" href="graduate.html">Graduate</a></li>
+              <li><a style="color: ${navColor}; font-weight: bold; font-size: 18px;" class="nav-link scrollto" href="join.html">Join Us</a></li>
             </ul>
             <i class="bi bi-list mobile-nav-toggle"></i>
           </nav><!-- .navbar -->
@@ -33,6 +40,7 @@ function loadHeader() {
     `;
     
     // Set active nav item based on current page
+    window.scrollTo(0, 0);
     setActiveNavItem();
   }
 }
